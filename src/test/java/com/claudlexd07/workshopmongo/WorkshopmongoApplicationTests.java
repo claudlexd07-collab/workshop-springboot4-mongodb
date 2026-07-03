1cd1a0ab6c8ff018dc6831c664c0d4a124b275a5
Claudemir A Dias <claudlexd07@gmail.com>
2026-07-03 17:14:55 -0300

    Project created

---
+++ src/test/java/com/claudlexd07/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudlexd07.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
