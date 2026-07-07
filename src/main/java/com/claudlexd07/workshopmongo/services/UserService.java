0e9f307d8bdc90bac7ae8c73995a8f5d1c5963d4
Claudemir A Dias <claudlexd07@gmail.com>
2026-07-07 11:49:03 -0300

    Obtendo um usuario por id

--- src/main/java/com/claudlexd07/workshopmongo/services/UserService.java
+++ src/main/java/com/claudlexd07/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.claudlexd07.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.claudlexd07.workshopmongo.domain.User;
 import com.claudlexd07.workshopmongo.repository.UserRepository;
+import com.claudlexd07.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired // Instancia um objeto automaticamente
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
