21880eda0cfac2ee94c57314a04d5b68857c222b
Claudemir A Dias <claudlexd07@gmail.com>
2026-07-08 12:30:17 -0300

    Consulta simples com query methods

--- src/main/java/com/claudlexd07/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/claudlexd07/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.claudlexd07.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.claudlexd07.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
